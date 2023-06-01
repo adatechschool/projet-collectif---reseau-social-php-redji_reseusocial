4805ee81bd1fe2d46bff646a8fd71b028d320e50
Justine <justine-lambert@hotmail.fr>
2023-06-01 10:29:24 +0200

    Replace user name based on the user_id

--- Niveau1/feed.php
+++ Niveau1/feed.php
@@ -37,13 +37,13 @@
                 $lesInformations = $mysqli->query($laQuestionEnSql);
                 $user = $lesInformations->fetch_assoc();
                 //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par l'alias et effacer la ligne ci-dessous
-                echo "<pre>" . print_r($user, 1) . "</pre>";
+                // echo "<pre>" . print_r($user, 1) . "</pre>";
                 ?>
                 <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
                 <section>
                     <h3>Présentation</h3>
                     <p>Sur cette page vous trouverez tous les message des utilisatrices
-                        auxquel est abonnée l'utilisatrice XXX
+                        auxquel est abonnée l'utilisatrice <?php echo $user['alias']; ?>
                         (n° <?php echo $userId ?>)
                     </p>
 
