2559b32907cad94361c81c4eb78cf643a031e7e3
Justine <justine-lambert@hotmail.fr>
2023-06-07 16:02:10 +0200

    Unlogged user cannot access wall and feed.

--- header.php
+++ header.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+if (isset($_SESSION['connected_id'])) {
+    $connectedId = $_SESSION['connected_id'];
+}
 ?>
 <!doctype html>
 <html lang="fr">
@@ -14,16 +17,21 @@ session_start();
             <a href='admin.php'><img src="resoc.jpg" alt="Logo de notre réseau social"/></a>
             <nav id="menu">
                 <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=5">Mur</a>
-                <a href="feed.php?user_id=5">Flux</a>
+            <?php if(isset($connectedId) && $connectedId!==""): ?>
+                <a href="wall.php?user_id=<?php echo $connectedId; ?>">Mur</a>
+                <a href="feed.php?user_id=<?php echo $connectedId; ?>">Flux</a>
+            <?php endif; ?>
                 <a href="tags.php?tag_id=1">Mots-clés</a>
             </nav>
+        <?php if(isset($connectedId) && $connectedId!==""): ?>
             <nav id="user">
                 <a href="#">▾ Profil</a>
                 <ul>
-                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
+                    <li><a href="settings.php?user_id=<?php echo $connectedId; ?>">Paramètres</a></li>
+                    <li><a href="followers.php?user_id=<?php echo $connectedId; ?>">Mes suiveurs</a></li>
+                    <li><a href="subscriptions.php?user_id=<?php echo $connectedId; ?>">Mes abonnements</a></li>
+                    <li><a href="logout.php">Se déconnecter</a></li>
                 </ul>
             </nav>
+        <?php endif; ?>
 </header>
